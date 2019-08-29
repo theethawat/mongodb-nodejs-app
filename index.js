ef5e6bc99b2bd19b82bcdb97697bf6280f654447
theethawat@linux <theethawat.s@outlook.co.th>
2019-08-29 11:49:50 +0700

    Passing 0ne DB by W3C tutorial by Frost

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const MongoClient = require("mongodb").MongoClient
 const assert = require("assert")
 
-const url =
-  "mongodb://192.168.2.11:27017,192.168.2.12:27017,192,168.2.13:27017/?replicaSet=rs"
-const dbName = "theethawatDB"
+const url = "mongodb://192.168.2.11:27017"
 
-const client = new MongoClient(url, { useNewUrlParser: true })
-
-try {
-  //Something Wrong
-  client.connect({ useUnifiedTopology: true })
-  const db = client.db(dbName)
-} catch (err) {
-  console.log(err.stack)
-}
-
-client.close()
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+  if (err) throw err
+  console.log("Hello World!!!   Database Found")
+  var dbo = db.db("theethawatDB")
+  dbo
+    .collection("family")
+    .find({})
+    .toArray(function(err, result) {
+      if (err) throw err
+      console.log(result)
+    })
+  db.close()
+})
